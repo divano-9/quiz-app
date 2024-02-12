ff2a3c83e178a742fe65c098fcaf5aa95731b9d4
divano-9 <dusanivanovic@mail.com>
2024-02-12 20:42:01 +0100

    typo fix

--- src/pages/QuizzApp.jsx
+++ src/pages/QuizzApp.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Context } from "../states/GlobalContext";
 import Form from "./Form";
-import Quiz from "./quiz";
+import Quiz from "./Quiz";
 
 const QuizzApp = () => {
   const { isSelecting } = useContext(Context);
